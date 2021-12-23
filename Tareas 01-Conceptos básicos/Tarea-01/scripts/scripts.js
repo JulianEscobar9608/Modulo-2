a8ab8da1486539a7d6a9276796733f35f4835826
JulianEscobar9608 <julian96081@gmail.com>
2021-12-23 14:12:46 -0500

    primer tarea

---
+++ Tareas 01-Conceptos básicos/Tarea-01/scripts/scripts.js
@@ -0,0 +1,17 @@
+/* Realizar un algoritmo en Javascript que permita calcular la suma, resta, multiplicación y
+ división de dos números, donde num1 = 5 y num2 = 7. Mostrar los resultados por consola. */
+
+ let num1 = 5;
+ let num2 = 7;
+ let suma,resta,producto,division;
+
+ suma = num1 + num2;
+ resta = num1 - num2;
+ producto = num1 * num2;
+ division = num1 / num2;
+
+ console.log(suma);
+ console.log(resta);
+ console.log(producto);
+ console.log(division);
+
